321233a7577d296db962ded0821da3f7cefa6bd8
Betsey Johnson <nseo@lls.edu>
2018-06-26 23:20:39 -0700

    initial

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("bacon cheese burger", false); 
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("texas burger", true);
+
+INSERT INTO burgers (burger_name, devoured) VALUES ("mexican breakfast burger", false); 
